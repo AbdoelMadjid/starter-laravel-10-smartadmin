1fc1533abc8792aec0eb1f21803f35acbf4b0158
abdoelmadjid <abdulmadjid.mpd@gmail.com>
2024-07-16 19:03:19 +0700

    updating Juli

--- resources/views/app_profiles/edit.blade.php
+++ resources/views/app_profiles/edit.blade.php
@@ -50,49 +50,70 @@
                             <form action="{{ route('app_profiles.update') }}" method="POST" enctype="multipart/form-data">
                                 @csrf
                                 @method('PUT')
-                                <div class="form-group">
-                                    <label>Nama:</label>
-                                    <input type="text" name="app_nama" class="form-control"
-                                        value="{{ $appProfile->app_nama }}" required>
-                                </div>
-                                <div class="form-group">
-                                    <label>Deskripsi:</label>
-                                    <textarea name="app_deskripsi" class="form-control" required>{{ $appProfile->app_deskripsi }}</textarea>
-                                </div>
-                                <div class="form-group">
-                                    <label>Tahun:</label>
-                                    <input type="number" name="app_tahun" class="form-control"
-                                        value="{{ $appProfile->app_tahun }}" required min="1900"
-                                        max="{{ date('Y') + 1 }}">
-                                </div>
-                                <div class="form-group">
-                                    <label>Pengembang:</label>
-                                    <input type="text" name="app_pengembang" class="form-control"
-                                        value="{{ $appProfile->app_pengembang }}" required>
-                                </div>
-                                <div class="form-group">
-                                    <label>Icon:</label>
-                                    <input type="file" name="app_icon" class="form-control">
-                                    <img src="{{ asset('admin/img/' . $appProfile->app_icon) }}" alt="Icon"
-                                        width="100">
-                                </div>
-                                <div class="form-group">
-                                    <label>Logo:</label>
-                                    <input type="file" name="app_logo" class="form-control">
-                                    <img src="{{ asset('admin/img/' . $appProfile->app_logo) }}" alt="Logo"
-                                        width="100">
-                                </div>
-                                <hr>
-                                <div class="height-8 d-flex flex-row align-items-center flex-wrap flex-shrink-0">
-                                    <a href="javascript:void(0);" class="btn btn-icon fs-xl width-1 mr-1"
-                                        data-toggle="tooltip" data-original-title="More options" data-placement="top">
-                                    </a>
+                                <x-col size1=6 size2=6>
+                                    <x-slot name="content1">
+                                        <div class="form-group">
+                                            <label>Nama:</label>
+                                            <input type="text" name="app_nama" class="form-control"
+                                                value="{{ $appProfile->app_nama }}" required>
+                                        </div>
+                                        <div class="form-group">
+                                            <label>Deskripsi:</label>
+                                            <textarea name="app_deskripsi" class="form-control" required>{{ $appProfile->app_deskripsi }}</textarea>
+                                        </div>
+                                        <div class="form-group">
+                                            <label>Tahun:</label>
+                                            <input type="number" name="app_tahun" class="form-control"
+                                                value="{{ $appProfile->app_tahun }}" required min="1900"
+                                                max="{{ date('Y') + 1 }}">
+                                        </div>
+                                        <div class="form-group">
+                                            <label>Pengembang:</label>
+                                            <input type="text" name="app_pengembang" class="form-control"
+                                                value="{{ $appProfile->app_pengembang }}" required>
+                                        </div>
+                                    </x-slot>
+                                    <x-slot name="content2">
+                                        <div class="form-group">
+                                            <x-col size1=6 size2=6>
+                                                <x-slot name="content1">
+                                                    <label>Icon:</label>
+                                                    <input type="file" name="app_icon" class="form-control">
+                                                </x-slot>
+                                                <x-slot name="content2">
+                                                    <img src="{{ asset('admin/img/' . $appProfile->app_icon) }}"
+                                                        alt="Icon" width="100">
+                                                </x-slot>
+                                            </x-col>
+                                        </div>
+                                        <div class="form-group">
+                                            <x-col size1=6 size2=6>
+                                                <x-slot name="content1">
+                                                    <label>Logo:</label>
+                                                    <input type="file" name="app_logo" class="form-control">
+                                                </x-slot>
+                                                <x-slot name="content2">
+                                                    <img src="{{ asset('admin/img/' . $appProfile->app_logo) }}"
+                                                        alt="Logo" width="100" class='txt-center'>
+                                                </x-slot>
+                                            </x-col>
+                                        </div>
+                                        <hr>
+                                        <div class="height-8 d-flex flex-row align-items-center flex-wrap flex-shrink-0">
+                                            <a href="javascript:void(0);" class="btn btn-icon fs-xl width-1 mr-1"
+                                                data-toggle="tooltip" data-original-title="More options"
+                                                data-placement="top">
+                                            </a>
 
-                                    <button type="submit" class="btn btn-info shadow-0 ml-auto">Update</button>
-                                </div>
-                                {{-- <div class="ml-auto">
-                                    <button type="submit" class="btn btn-primary">Update</button>
-                                </div> --}}
+                                            <button type="submit" class="btn btn-info shadow-0 ml-auto">Update</button>
+                                        </div>
+                                        {{--
+                                            <div class="ml-auto">
+                                                <button type="submit" class="btn btn-primary">Update</button>
+                                            </div>
+                                        --}}
+                                    </x-slot>
+                                </x-col>
                             </form>
                         </div>
                     </div>
--- resources/views/app_profiles/show.blade.php
+++ resources/views/app_profiles/show.blade.php
@@ -53,32 +53,42 @@
                     </div>
                 </x-panel.tool-bar>
             </x-slot>
-            <table class="table table-bordered">
-                <tr>
-                    <th>Nama</th>
-                    <td>{{ $appProfile->app_nama }}</td>
-                </tr>
-                <tr>
-                    <th>Deskripsi</th>
-                    <td>{{ $appProfile->app_deskripsi }}</td>
-                </tr>
-                <tr>
-                    <th>Tahun</th>
-                    <td>{{ $appProfile->app_tahun }}</td>
-                </tr>
-                <tr>
-                    <th>Pengembang</th>
-                    <td>{{ $appProfile->app_pengembang }}</td>
-                </tr>
-                <tr>
-                    <th>Icon</th>
-                    <td><img src="{{ asset('admin/img/' . $appProfile->app_icon) }}" alt="Icon" width="50"></td>
-                </tr>
-                <tr>
-                    <th>Logo</th>
-                    <td><img src="{{ asset('admin/img/' . $appProfile->app_logo) }}" alt="Logo" width="50"></td>
-                </tr>
-            </table>
+            <x-col size1=6 size2=6>
+                <x-slot name="content1">
+                    <table class="table table-bordered">
+                        <tr>
+                            <th>Nama</th>
+                            <td>{{ $appProfile->app_nama }}</td>
+                        </tr>
+                        <tr>
+                            <th>Deskripsi</th>
+                            <td>{{ $appProfile->app_deskripsi }}</td>
+                        </tr>
+                        <tr>
+                            <th>Tahun</th>
+                            <td>{{ $appProfile->app_tahun }}</td>
+                        </tr>
+                        <tr>
+                            <th>Pengembang</th>
+                            <td>{{ $appProfile->app_pengembang }}</td>
+                        </tr>
+                    </table>
+                </x-slot>
+                <table class="table table-bordered">
+                    <tr>
+                        <th>Icon</th>
+                        <td><img src="{{ asset('admin/img/' . $appProfile->app_icon) }}" alt="Icon" width="50">
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>Logo</th>
+                        <td><img src="{{ asset('admin/img/' . $appProfile->app_logo) }}" alt="Logo" width="50">
+                        </td>
+                    </tr>
+                </table>
+                <x-slot name="Content2">
+                </x-slot>
+            </x-col>
             <x-slot name="panelcontentfoot">
                 <x-button href="{{ route('app_profiles.edit') }}" size="sm" :label="__('Edit')" class="ml-auto" />
             </x-slot>
